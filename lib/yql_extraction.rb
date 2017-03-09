ce7c01f9b6282c84096ef1beeb78ca5b3364a65e
Michael Becco <e4e6d4d5@gmail.com>
2017-03-09 08:07:11 -0800

    Convert vcr_cassette to json, test yql_extraction#current_value_is_maximum?

--- lib/yql_extraction.rb
+++ lib/yql_extraction.rb
@@ -1,2 +1,16 @@
-class YqlExtracton
+class YqlExtraction
+
+  def current_value_is_maximum?(arr)
+    arr.max == arr.last
+  end
+
+  def parse_queried_json json
+    arr = []
+    p json
+  end
+
+  def sample_method hash
+    # placeholder for method to extract Volume
+    hash.dig("query","results","quote","Volume")
+  end
 end
--- test/yql_extraction_test.rb
+++ test/yql_extraction_test.rb
@@ -3,6 +3,7 @@ require 'minitest/autorun'
 require 'yql_extraction'
 require 'vcr'
 require 'date'
+require 'json'
 
 VCR.configure do |config|
   config.cassette_library_dir = "vcr_cassettes"
@@ -13,19 +14,38 @@ class YqlExtractionTest < Minitest::Test
   def setup
     symbol = 'YHOO'
     current_date = Time.now.strftime("%Y-%m-%d")
-    date_lookback = (DateTime.now - 365).strftime("%Y-%m-%d")
+    date_lookback = (DateTime.now - 1).strftime("%Y-%m-%d")
     @url = "http://query.yahooapis.com/v1/public/yql?q="
     @select_statment = "SELECT * FROM yahoo.finance.historicaldata"
     @where_statement = " WHERE symbol =\"#{symbol}\" AND startDate =\"#{date_lookback}\" AND endDate =\"#{current_date}\""
     @format = "&format=json&diagnostic=true&env=store://datatables.org/alltableswithkeys&callback="
+    @yql = YqlExtraction.new
   end
 
   def test_response_code_200
-    url =   @url + @select_statment + @where_statement + @format
+    url = @url + @select_statment + @where_statement + @format
     VCR.use_cassette('yql_query') do
       response = Net::HTTP.get_response(URI("#{url}"))
       assert_equal "200", response.code
     end
   end
-end
 
+  def test_format_data_to_json
+    url = @url + @select_statment + @where_statement + @format
+    VCR.use_cassette('query', :serialize_with => :json) do
+      response = Net::HTTP.get_response(URI("#{url}"))
+      hash_response = JSON.parse(response.body)
+      assert_equal Hash, hash_response.class
+    end
+  end
+
+  def test_maximum_value_returned
+    arr = (1..252).to_a
+    assert_equal true, @yql.current_value_is_maximum?(arr)
+  end
+
+  def test_maximum_value_is_not_current_value
+    arr = [6, 7, 10, 3]
+    assert_equal false, @yql.current_value_is_maximum?(arr)
+  end
+end
